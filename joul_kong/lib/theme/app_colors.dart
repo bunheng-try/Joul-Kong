60665f6fa19e6b973709b40b4c8fcc51dd673e54
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-04-18 22:00:12 +0700

    feat: add core theme classes (colors, spacing, radius)

---
+++ joul_kong/lib/theme/app_colors.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  static const Color primary = Color(0xFF4F39F6);
+  static const Color cardBlue = Color(0xFF5744DC);
+
+  static const Color backgroundColor = Color(0xFFF9FAFB);
+
+  static const Color dayPass = Color(0xFF155DFC);
+  static const Color monthPass = Color(0xFF4F39F6);
+  static const Color yearPass = Color(0xFFF0B100);
+
+  static const Color cardBgGreen = Color(0xFFEDFDF5);
+  static const Color cardBgBlue = Color(0xFFEFF6FF);
+  static const Color cardBgRed = Color(0xFFFFF7ED);
+
+  static const Color black = Color(0xFF101828);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color darkGrey = Color(0xFF4A5565);
+  static const Color grey = Color(0xFF6A7282);
+  static const Color lightGrey = Color(0xFF99A1AF);
+
+  static const Color mute = Color(0xFFE5E7EB);
+
+  static const Color darkGreen = Color(0xFF0D542B);
+  static const Color green = Color(0xFF008236);
+  static const Color lightGreen = Color(0xFF00A63E);
+
+  static const Color darkBlue = Color(0xFF1C398E);
+  static const Color blue = Color(0xFF1C398E);
+  static const Color lightBlue = Color(0xFF155DFC);
+
+  static const Color darkRed = Color(0xFF7E2A0C);
+  static const Color red = Color(0xFFCA3500);
+  static const Color lightRed = Color(0xFFF54900);
+}
