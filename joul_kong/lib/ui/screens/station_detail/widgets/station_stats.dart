ca084ffe6ea08ea52cd99e93942056b701c4a120
BN2323 <Bunnaroth.tha@student.cadt.edu.kh>
2026-04-20 18:09:17 +0700

    implement station view screen

---
+++ joul_kong/lib/ui/screens/station_detail/widgets/station_stats.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:joul_kong/ui/screens/station_detail/view_model/station_detail_view_model.dart';
+import 'package:joul_kong/ui/screens/station_detail/widgets/stat_card.dart';
+
+class StationStats extends StatelessWidget {
+  final StationDetailViewModel vm;
+
+  const StationStats({super.key, required this.vm});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        StatCard(
+          icon: Icons.pedal_bike,
+          value: "${vm.getAvailableBikeCount()}",
+          label: "Available",
+          color: const Color(0xFF7BF1A8),
+        ),
+
+        StatCard(
+          icon: Icons.local_parking,
+          value: "${vm.emptySlotCount}",
+          label: "Parks",
+          color: const Color(0xFF6EC6FF),
+        ),
+
+        StatCard(
+          icon: Icons.map,
+          value: "--",
+          label: "Distance",
+          color: const Color(0xFFB0BEC5),
+        ),
+      ],
+    );
+  }
+}
